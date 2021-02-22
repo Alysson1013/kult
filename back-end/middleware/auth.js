70856840f801416d86d2169b7c682bf60c9295c8
Alysson Eduardo <alysson6801@hotmail.com>
2021-02-22 18:01:05 -0300

    (22/02) Middleware.

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -1,2 +1,28 @@
+require('dotenv').config()
 const jwt = require("jsonwebtoken")
-const secret = "adfsadsadsad"
+const secret = process.env.SECRET
+
+module.exports = function(req, res, next){
+    const authToken = req.headers['authorization']
+
+    if(authToken != undefined){
+        const bearer = authToken.split(' ')
+        const token = bearer[1]
+
+        try{
+            const decoded = jwt.verify(token, secret)
+            if(decoded.role == 1){
+                console.log(decoded)
+                next()
+            } else {
+                res.send("You arent a Administrator!!!")
+            }
+        } catch(error){
+            res.status(403)
+            res.send("You are not authenticated")
+        }
+    } else {
+        res.status(403)
+        res.send("You are not authenticated")
+    }
+}
